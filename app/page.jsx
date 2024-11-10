7b99de868732e05a60fb1666a998332c64836454
Brajy01 <113512782+Brajy01@users.noreply.github.com>
2024-11-10 12:12:55 +0100

    connexion page:  MVP ok

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,45 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Test
+    <div className="flex justify-center items-center min-h-screen">
+      <Card>
+        <CardHeader>
+          <CardTitle>Page de connection</CardTitle>
+          <CardDescription>
+            Entrer votre e-mail et votre mot de passe pour accéder à votre
+            compte
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Label htmlFor="email">E-mail</Label>
+          <Input
+            type="email"
+            id="email"
+            placeholder="Saisissez votre e-mail..."
+          ></Input>
+
+          <Label htmlFor="email">Mot de passe</Label>
+          <Input
+            type="password"
+            id="password"
+            placeholder="Saisissez votre mot de passe..."
+          ></Input>
+        </CardContent>
+        <CardFooter className="flex justify-center">
+          <Button>Valider</Button>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
